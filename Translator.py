c02d8f7eee57874627203fa4b04e38b0ce1896a1
Andres Rivera Marquez <andres.rivera@iteso.mx>
2021-10-15 13:26:28 -0700

    feat: Translator.py code finished, pre-tests
    First completed version of the code Translator.
    
    	--Added some dictionaries that controls the position of the arguments
    	--Added some dictionaries for the binary format of the arguments.
    	--Added an algorithm that appends the translated code to the returned
    	list.
    	--The dictionary of mnemRegs has been split into two
    	individual dictionaries.
    
    This version hasn't been tested yet.

--- Translator.py
+++ Translator.py
@@ -16,14 +16,14 @@ def translate(list code):
 
     #Here are defined some dictionaries that will have the mnemonics and
     #registers with their respective value.
-    mnemRegs = {"add": 0, "addi": 1, "and": 2, "andi": 3,
-                "beq": 4, "bne": 5, "j": 6, "jal": 7,
-                "jr": 10, "lb": 11, "or": 12, "sb": 13, 
-                "sll": 14, "srl": 15, "x0": 0,"x1": 1,
-                "x2": 2, "x3": 3, "x4": 4, "x5": 5,"x6": 6,
-                "x7": 7}
-    orderAppend = {"std1" : [2, 3, 1], "std2" : [3, 2, 1], "off1" : [1, 2],
-                "off2" : [3, 1], "imm0" : [2, 1]}
+    mnemos = {"add": 0, "addi": 1, "and": 2, "andi": 3, "beq": 4, "bne": 5,
+              "j": 6, "jal": 7, "jr": 10, "lb": 11, "or": 12, "sb": 13, 
+              "sll": 14, "srl": 15}
+    regs = {"x0": 0,"x1": 1, "x2": 2, "x3": 3, "x4": 4, "x5": 5,"x6": 6,
+            "x7": 7, "C2": 0, "zero": 0}
+    orderAppend = {"std1" : [2, 3, 1, 4], "std2" : [3, 2, 1, 4], 
+            "off1" : [1, 2, 3], "off2" : [3, 1, 2], "imm0" : [2, 1, 3]}
+    binformat = {"type1" : [4,3,3,3,5], "type2" : [4,3,3,8]}
 
     #Dictionary that will contain the labels of the code, if there's any
     labels = {}
@@ -37,7 +37,7 @@ def translate(list code):
         pc += 1
         islabel = line.split(":")
         if (len(islabel) == 2):
-            labels[islabel[0]] = pc
+            labels[islabel[0].strip()] = pc
 
     #It's necessary to identify first the labels.
     #In this cycle, the program will iterate the lines of code 
@@ -47,7 +47,7 @@ def translate(list code):
         #This part of the code will split the line into arguments.
         #Eliminating the labels
         pc += 1
-        instr = line.split(":")
+        instr = instr.split(":")
         if (len(instr) == 2):
             instr = instr[1]
         instr = instr.split(",")
@@ -59,49 +59,98 @@ def translate(list code):
 
         #Here, the code will process the arguments to create a list
         #with the equivalent values of the arguments
-        codet.append( mnemRegs.get( instr[0], "Abscent") )
-        
-        if (codet[0] == "Abscent"):
+        if (instr[0] in mnemos):
+            codet.append( mnemos[ instr[0] ] )
+        else:
             print(f"Error en la linea: {pc}, no se reconoce la operacion")
             return 0
 
-        elif (len(instr) == 2):
+        if (len(instr) == 2):
 
-            if (codet[0] == 6 or codet[0] == 7):
+            if (codet[0] in [6,7]):
+                codet.append(0)
+                codet.append(0)
                 codet.append( labels.get( instr[1] ) ) 
             elif (codet[0] == 10):
-                codet.append( mnemRegs.get( instr[1] ) ) 
+                codet.append( regs.get( instr[1] ) ) 
+                codet.append(0)
+                codet.append(0)
+
                 
             else:
                 print(f"Faltan argumentos en la linea {pc}")
                 return 0
         
         elif (len(instr) == 4):
-            if (codet[0] == 0 or codet[0] == 2 or codet[0] == 12):
+            #If equal to an add, and or or instruction, std1 pattern is added
+            if (codet[0] in [0, 2, 12]):
                 corris = orderAppend("std1")
+                instr.append("zero")
 
-            elif (codet[0] == 14 or codet[0] == 15):
+            #If equal to sll or srl instruction, std2 pattern is added.
+            elif (codet[0] in [14, 15]):
                 corris = orderAppend("std2")
+                instr.append("zero")
 
-            elif (codet[0] == 4 or codet[0] == 5):
+            #If equal to beq or bne instruction, off1 pattern is added.
+            elif (codet[0] in [4, 5]):
                 corris = orderAppend("off1")
+                regs["C2"] = (-1 * labels.get(instr[3])) & (2**8-1)
+                instr[3] = "C2"
 
-            elif (codet[0] == 11 or codet[0] == 13):
+            #If equal to lb or sb instruction, off2 pattern is added.
+            elif (codet[0] in [11, 13] ):
                 corris = orderAppend("off2")
+                regs["C2"] = (-1 * labels.get(instr[2])) & (2**8-1)
+                instr[2] = "C2"
 
-            elif (codet[0] == 1 or codet[0] == 3):
+            #If equal to addi or andi instruction, imm0 pattern is added.
+            elif (codet[0] in [1, 3]):
                 corris = orderAppend("imm0")
+                regs["C2"] = eval(instr[3])
+                if regs["C2"] < 0:
+                    regs["C2"] = regs["C2"] & (2**8-1)
+                instr[3] = "C2"
 
             else:
                 print(f"Se ha cometido un error en la linea {pc}")
                 return 0
 
             for z in corris:
-                codet.append( instr[z] )
-        elif ():
-            print(f"Error: Revise la linea de codigo {pc}")
+                codet.append( regs.get( instr[z] ) )
 
+            #At this point we have a decoded list with the opcodes and
+            #values of registers, labels and constants in decimal base.
+            #Now the next part is to transform them into binary code.
+            if codet[0] in [0,2,12,14,15]:
+                patt = binformat["type1"]
+            else codet[0] in [1,3,4,5,6,7,10,11,13]):
+                patt = binformat["type2"]
+
+            binarytext = ""
+            i = 0
+            while (i < len(codet) ):
+                binarytext += "{:0{patt[i]}b}".format(codet[i])
+                i++
+
+            ensa.append(binarytext)
+
+        else:
+            print(f"Error: Revise la linea de codigo {pc}")
+    
+    return ensa
 
 if __name__ == "__main__":
-    #ejecuta funcion
+    codigo_prueba = ["addi,x1,x0,1",
+                     "addi,x2,x0,0x02",
+                     "addi,x3,x0,0xA0",
+                     "addi,x4,x0,-1",
+                     "add,x6,x2,x1",
+                     "and,x7,x4,x3",
+                     "or,x5,x3,x4",
+                     "sll,x3,x3,x1",
+                     "srl,x3,x3,x1"]
+    
+    print(translate(codigo_prueba))
+
 
