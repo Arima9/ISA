9b097d0d50acc6b364905354c7b158561ba5ea76
Jose Leonardo Quiñonez <leonardoquinonez98@gmail.com>
2021-10-15 20:05:03 -0500

    Correction of module 1 Reading_files
    Second version of the module 1
    	--Changes in some lines to add the arguments to the function
    	--Added a line that deletes the \n on the lines read on the file
    	--Deleted the for cycle to make a more understandable code

--- Reading_file.py
+++ Reading_file.py
@@ -4,19 +4,21 @@
 # :indentSize=4:tabSize=8:noTabs=true:
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-# This program reads a file that the user want to transform (codigo1.txt,
-# codigo2.txt, codigo3.txt, codigo4.txt) and returns a tupple with the content of
-# the file read
-def lectura_archivo():
-    import sys #Module that returns a list with all the arguments of a file"
+""" 
+    This program reads a file that the user want to transform (codigo1.txt,
+    codigo2.txt, codigo3.txt, codigo4.txt) and returns a tupple with the 
+    content of  the file read.
+"""
+def lectura_archivo(nombre):
+    #Module that returns a list with all the arguments of a file"
 
-    nombre_archivo = sys.argv #File that we want to read"
-    valores = open(nombre_archivo[1])
+    nombre_archivo = nombre      #File that we want to read"
+    valores = open(nombre_archivo)
     lineas = valores.readlines()
-    print(len(lineas)) 
-    for x in lineas:
-        print(x,end='')
+    lineas = list(map(lambda l: l.rstrip('\n'), lineas))
     return lineas
+
 if __name__ == "__main__":
-    lectura_archivo()
+
+    print(lectura_archivo("codigo1.txt"))
 
